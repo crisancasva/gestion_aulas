dbe305d173af26544aeed77e0b8efbe63c637383
Cristhian Castaño <crisan1988@hotmail.com>
2026-06-26 21:28:06 -0500

    first commit

---
+++ src/main/java/com/crisan/gestion_aulas/GestionAulasApplication.java
@@ -0,0 +1,13 @@
+package com.crisan.gestion_aulas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionAulasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionAulasApplication.class, args);
+	}
+
+}
